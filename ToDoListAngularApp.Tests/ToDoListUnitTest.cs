1c0090af85904a1966914a1c3630debae9de8978
Amit Shahi <ami.oneworld@gmail.com>
2019-09-16 22:09:05 +0530

    Excluded Test from Azure Build Process

--- ToDoListAngularApp.Tests/ToDoListUnitTest.cs
+++ ToDoListAngularApp.Tests/ToDoListUnitTest.cs
@@ -13,80 +13,80 @@ namespace ToDoListAngularApp.Tests
 {
     public class ToDoListControllerTest : IDisposable
     {
-        private readonly TodoService _todoService;
-        protected readonly TodoDbContext _context;
-        public ToDoListControllerTest()
-        {
-            var options = new DbContextOptionsBuilder<TodoDbContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
+        // private readonly TodoService _todoService;
+        // protected readonly TodoDbContext _context;
+        // public ToDoListControllerTest()
+        // {
+        //     var options = new DbContextOptionsBuilder<TodoDbContext>()
+        //         .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+        //         .Options;
 
-            _context = new TodoDbContext(options);
+        //     _context = new TodoDbContext(options);
 
-            _context.Database.EnsureCreated();
+        //     _context.Database.EnsureCreated();
 
-            // this.Initialize(_context);
+        //     // this.Initialize(_context);
 
-            this._todoService = new TodoService();
+        //     this._todoService = new TodoService();
 
-        }
+        // }
  
         //[Fact]
-        public async Task Add_New_Item_Should_Return_Integer_Value()
-        {
+        // public async Task Add_New_Item_Should_Return_Integer_Value()
+        // {
 
-            var controller = new ToDoListController(this._todoService);
+        //     var controller = new ToDoListController(this._todoService);
 
-            // var result = await controller.PostCustomer(new Customer { Id = 7, Name = "Scary Terry" });
+        //     // var result = await controller.PostCustomer(new Customer { Id = 7, Name = "Scary Terry" });
 
-            var result = await controller.AddTodo(new TodoAddViewModel
-            {
-                WorkTodo = "Item1",
-                IsCompleted = false
-            });//.ConfigureAwait(false);
+        //     var result = await controller.AddTodo(new TodoAddViewModel
+        //     {
+        //         WorkTodo = "Item1",
+        //         IsCompleted = false
+        //     });//.ConfigureAwait(false);
 
-            // Assert.NotNull(result);
-
-
-            //  var result = await _todoService.Add(new Todo
-            //    {
-            //        WorkTodo = "Item1",
-            //        IsCompleted = false
-            //    }).ConfigureAwait(false); 
+        //     // Assert.NotNull(result);
 
 
-            //Console.WriteLine(result);
+        //     //  var result = await _todoService.Add(new Todo
+        //     //    {
+        //     //        WorkTodo = "Item1",
+        //     //        IsCompleted = false
+        //     //    }).ConfigureAwait(false); 
 
-            //Assert.Equals("1", "1");
-            // SynchronizationContext.Current.Post(state => { }, null);
 
-            // await Task.Yield();
+        //     //Console.WriteLine(result);
+
+        //     //Assert.Equals("1", "1");
+        //     // SynchronizationContext.Current.Post(state => { }, null);
+
+        //     // await Task.Yield();
             
-            Assert.IsType<OkObjectResult>(result);
-            
-            //Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        //     Assert.IsType<OkObjectResult>(result);
+
+        //     //Assert.That(result, Is.InstanceOf<OkObjectResult>());
 
 
 
-            //Assert.AreEqual(result, BadRequest());
-        }
+        //     //Assert.AreEqual(result, BadRequest());
+        // }
 
 
         public void Initialize(TodoDbContext context)
         {
-            if (context.Todos.Any())
-            {
-                return;
-            }
+            // if (context.Todos.Any())
+            // {
+            //     return;
+            // }
 
             // Seed(context);
         }
 
         public void Dispose()
         {
-            _context.Database.EnsureDeleted();
+            // _context.Database.EnsureDeleted();
 
-            _context.Dispose();
+            // _context.Dispose();
         }
     }
 }
